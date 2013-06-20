afbd62c98f25d400ef714034b02b61a9252c7c33
Simon Fakir <simon@fakir-it.de>
2013-06-20 11:07:22 +0200

    base setup

---
+++ languages/de/modules.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   DistributorMapBundle
+ * @author    Simon Fakir
+ * @license   LGPL
+ * @copyright Fakir Informatik 2013
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD'][''] = array('', '');
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD'][''] = array('', '');
---
+++ languages/en/default.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   DistributorMapBundle
+ * @author    Simon Fakir
+ * @license   LGPL
+ * @copyright Fakir Informatik 2013
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC'][''] = '';
